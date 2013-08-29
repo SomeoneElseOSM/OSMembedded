d37061cc033dc3514b12bc1e2994f3153b1d5614
Andy Townsend <ajtown@atownsend.org.uk>
2013-08-29 11:21:06 +0100

    Initial version of "OSMembedded"
    Largely based on the canonical Leaflet / Switch2osm example at http://switch2osm.org/using-tiles/getting-started-with-leaflet/
    
    Has been made packagable as a Blackberry WebWorks application.
    Currently supports standard-format OSM tiles embedded below
    "images" (only two layers included here; generate more via http://switch2osm.org/serving-tiles/manually-building-a-tile-server-12-04/)
    and OSM "standard" tiles on the website.
    
    See leaflet_embed.js for details of how to access tiles within the
    shared documents folder and on the SD card.
    
    Start location defaults to the centre of Matlock -
    it's designed to be changed to something local.

---
+++ Scripts/leaflet_embed.js
@@ -0,0 +1,57 @@
+// A very simple leaflet-based map based on the example at:
+// http://switch2osm.org/using-tiles/getting-started-with-leaflet/
+//
+// See also:
+// http://leafletjs.com/examples.html
+
+var map;
+
+function initmap() {
+    // Tiles layers embedded in the application, in "documents" in device memory, 
+    // on the SD Card, and read from the OSM standard map on the web.
+
+    // Layers for device memory and on the SD card are currently commented out (here and below)
+    // as they're not present by default
+
+    var embUrl='images/osm_tiles/{z}/{x}/{y}.png';
+//    var devUrl='file:///accounts/1000/shared/documents/osm_tiles/{z}/{x}/{y}.png';
+//    var sdcUrl='file:///accounts/1000/removable/sdcard/documents/osm_tiles/{z}/{x}/{y}.png';
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+
+    var osmAttrib='Map data &copy; OpenStreetMap contributors';
+
+    // maxZoom is restricted to 16 on the embedded layer as we haven't packaged zoom levels
+    // more that that within the application.
+
+    var embLayer = new L.TileLayer(embUrl, {minZoom: 6, maxZoom: 16, attribution: osmAttrib});
+//    var devLayer = new L.TileLayer(devUrl, {minZoom: 6, maxZoom: 18, attribution: osmAttrib});
+//    var sdcLayer = new L.TileLayer(sdcUrl, {minZoom: 6, maxZoom: 18, attribution: osmAttrib});
+    var osmLayer = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 18, attribution: osmAttrib});
+
+    var placesLayer = new L.LayerGroup();
+
+    // Create a map centred on Matlock.
+    // There's no need for a zoom control as we can use pinch to zoom.
+
+    var map = new L.Map('map', {
+	    center: new L.LatLng(53.1374, -1.5530),
+	    zoom: 15,
+	    layers: [ embLayer ],
+	    zoomControl: false
+	});
+
+    var baseMaps = {
+	"In App":       embLayer,
+//	"On Device":    devLayer,
+//	"SD Card":      sdcLayer,
+    };
+
+    var layersControl = new L.Control.Layers( baseMaps );
+
+// It's possible to add layers programatically, 
+// but not easy to check if directories exist before doing so:
+
+    layersControl.addBaseLayer( osmLayer, "OSM Online" );
+    map.addControl(layersControl);
+
+}
